11f8c2eaa0bf394b13ffb05cdb9f1056d2bb082c
Zubayer204 <zubayer.skd@gmail.com>
2021-03-27 17:20:14 +0600

    fully built the website

--- main.py
+++ main.py
@@ -1,15 +1,26 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from cloudscraper import CloudScraper
+import bs4
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods=['GET', 'POST'])
 def home():
-	return render_template("index.html")
+	if request.method == "GET":
+		return render_template("index.html", show_tutorial=True)
 
+	link = request.form['url']
+	sc = CloudScraper()
+	res = sc.get(link)
+	soup = bs4.BeautifulSoup(res.content, 'html5lib')
+	download_link = soup.find("input", attrs={"type": "hidden", "name": "FU"})
+	try:
+		download_link = download_link['value']
+		return render_template("index.html", show_tutorial=False, download_link=download_link)
+	except:
+		download_link = "Nan"
+		return render_template("index.html", show_tutorial=False, download_link=download_link)
 
-@app.route("/googlec935ff44bf4fedc0.html")
-def google():
-	return render_template("googlec935ff44bf4fedc0.html")
 
 
 if __name__ == "__main__":
