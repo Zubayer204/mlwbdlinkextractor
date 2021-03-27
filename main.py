dbcfed91a5a8bb6372906e6b88ec35d47c2d5021
Zubayer204 <zubayer.skd@gmail.com>
2021-03-27 13:33:37 +0600

    added index.html

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-	return "<h1>Successfully setup flask in python anywhere</h1>"
+	return render_template("index.html")
 
 
 if __name__ == "__main__":
