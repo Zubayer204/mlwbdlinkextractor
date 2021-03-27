718433d7f1db57fab7cb9f31b20a7f337f9829b9
Zubayer204 <zubayer.skd@gmail.com>
2021-03-27 12:38:42 +0600

    initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+	return "<h1>Successfully setup flask</h1>"
+
+
+if __name__ == "__main__":
+	app.run(debug=False, host="0.0.0.0")
