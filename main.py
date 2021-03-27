1515614e3bacdb72f41f1a69f22ea664a91180cb
Zubayer204 <zubayer.skd@gmail.com>
2021-03-27 13:41:51 +0600

    added google site verification

--- main.py
+++ main.py
@@ -7,5 +7,10 @@ def home():
 	return render_template("index.html")
 
 
+@app.route("/googlec935ff44bf4fedc0.html")
+def google():
+	return render_template("googlec935ff44bf4fedc0.html")
+
+
 if __name__ == "__main__":
 	app.run(debug=False, host="0.0.0.0")
